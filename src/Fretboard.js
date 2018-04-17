ae901cccb6738ac3430ce73e3b3b152c8192481f
Eetu Latja <eetu.latja@gmail.com>
2018-04-17 09:50:45 +0300

    Indentation to spaces

--- src/Fretboard.js
+++ src/Fretboard.js
@@ -6,92 +6,92 @@ import { getScalePositionsOnFretboard } from './scales';
 
 export default class Fretboards extends PureComponent {
 
-	render() {
-		const numberOfStrings = 6;
-		const numberOfFrets = 24;
+    render() {
+        const numberOfStrings = 6;
+        const numberOfFrets = 24;
 
-		const markerDotFrets = [
-			3,
-			5,
-			7,
-			9,
-			12,
-			15,
-			17,
-			19,
-			21,
-			24,
-		];
+        const markerDotFrets = [
+            3,
+            5,
+            7,
+            9,
+            12,
+            15,
+            17,
+            19,
+            21,
+            24,
+        ];
 
-		const displayedNotes = getScalePositionsOnFretboard(this.props.scale, this.props.rootNote);
+        const displayedNotes = getScalePositionsOnFretboard(this.props.scale, this.props.rootNote);
 
-		return (
-			<div style={styles.fretboard}>
-				{markerDotFrets.map(markerFret => {
-					const top = `${fretHeight * (numberOfStrings - 1) / 2 - markerDiameter / 2}rem`;
-					const left = `${(markerFret - 1) * fretWidth + (fretWidth - markerDiameter) / 2}rem`;
+        return (
+            <div style={styles.fretboard}>
+                {markerDotFrets.map(markerFret => {
+                    const top = `${fretHeight * (numberOfStrings - 1) / 2 - markerDiameter / 2}rem`;
+                    const left = `${(markerFret - 1) * fretWidth + (fretWidth - markerDiameter) / 2}rem`;
 
-					if (markerFret % 12 !== 0) {
-						return (
-							<div
-								key={markerFret}
-								style={{
-									...styles.markerDot,
-									top,
-									left,
-								}}
-							/>
-						);
-					}
-					else {
-						return (
-							<div
-								key={markerFret}
-								style={{
-									...styles.doubleMarkerDot,
-									top,
-									left,
-								}}
-							>
-								<div
-									style={{
-										...styles.markerDot,
-										top: `${-fretHeight}rem`,
-									}}
-								/>
-								<div
-									style={{
-										...styles.markerDot,
-										top: `${fretHeight}rem`,
-									}}
-								/>
-							</div>
-						);
-					}
-				})}
-				{_.range(1, numberOfStrings + 1).map(string => (
-					<div key={string} style={styles.string}>
-						{_.range(numberOfFrets + 1).map(fret => (
-							<div
-								key={fret}
-								style={{
-									...styles.fret,
-									...(string === numberOfStrings - 1 && styles.secondLowestStringFret),
-									...(string === numberOfStrings && styles.lowestStringFret),
-									...(fret === numberOfFrets - 1 && string !== numberOfStrings && styles.secondHighestFret),
-									...(fret === numberOfFrets && styles.highestFret),
-								}}
-							>
-								{displayedNotes[string] && displayedNotes[string][fret] &&
-									<div style={styles.noteMarker} />
-								}
-							</div>
-						))}
-					</div>
-				))}
-			</div>
-		);
-	}
+                    if (markerFret % 12 !== 0) {
+                        return (
+                            <div
+                                key={markerFret}
+                                style={{
+                                    ...styles.markerDot,
+                                    top,
+                                    left,
+                                }}
+                            />
+                        );
+                    }
+                    else {
+                        return (
+                            <div
+                                key={markerFret}
+                                style={{
+                                    ...styles.doubleMarkerDot,
+                                    top,
+                                    left,
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        ...styles.markerDot,
+                                        top: `${-fretHeight}rem`,
+                                    }}
+                                />
+                                <div
+                                    style={{
+                                        ...styles.markerDot,
+                                        top: `${fretHeight}rem`,
+                                    }}
+                                />
+                            </div>
+                        );
+                    }
+                })}
+                {_.range(1, numberOfStrings + 1).map(string => (
+                    <div key={string} style={styles.string}>
+                        {_.range(numberOfFrets + 1).map(fret => (
+                            <div
+                                key={fret}
+                                style={{
+                                    ...styles.fret,
+                                    ...(string === numberOfStrings - 1 && styles.secondLowestStringFret),
+                                    ...(string === numberOfStrings && styles.lowestStringFret),
+                                    ...(fret === numberOfFrets - 1 && string !== numberOfStrings && styles.secondHighestFret),
+                                    ...(fret === numberOfFrets && styles.highestFret),
+                                }}
+                            >
+                                {displayedNotes[string] && displayedNotes[string][fret] &&
+                                    <div style={styles.noteMarker} />
+                                }
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
 }
 
@@ -101,64 +101,64 @@ const fretHeight = 2;
 const markerDiameter = 1.5;
 
 const styles = {
-	fretboard: {
-		position: 'relative',
+    fretboard: {
+        position: 'relative',
 
-		display: 'flex',
-		flexDirection: 'column',
-	},
-	markerDot: {
-		position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    markerDot: {
+        position: 'absolute',
 
-		width: `${markerDiameter}rem`,
-		height: `${markerDiameter}rem`,
+        width: `${markerDiameter}rem`,
+        height: `${markerDiameter}rem`,
 
-		backgroundColor: 'gray',
-		borderRadius: '100%',
-	},
-	doubleMarkerDot: {
-		position: 'absolute',
-	},
-	string: {
-		display: 'flex',
-	},
-	fret: {
-		position: 'relative',
+        backgroundColor: 'gray',
+        borderRadius: '100%',
+    },
+    doubleMarkerDot: {
+        position: 'absolute',
+    },
+    string: {
+        display: 'flex',
+    },
+    fret: {
+        position: 'relative',
 
-		boxSizing: 'border-box',
+        boxSizing: 'border-box',
 
-		width: `${fretWidth}rem`,
-		height: `${fretHeight}rem`,
+        width: `${fretWidth}rem`,
+        height: `${fretHeight}rem`,
 
-		borderTopStyle: 'solid',
-		borderLeftStyle: 'solid',
-		borderColor: 'black',
-		borderWidth: 1,
-	},
-	secondLowestStringFret: {
-		borderBottomStyle: 'solid',
-	},
-	lowestStringFret: {
-		borderTopStyle: 'none',
-		borderLeftStyle: 'solid',
-		borderLeftColor: 'rgba(0, 0, 0, 0)',
-	},
-	secondHighestFret: {
-		borderRightStyle: 'solid',
-	},
-	highestFret: {
-		borderStyle: 'none',
-	},
-	noteMarker: {
-		position: 'absolute',
+        borderTopStyle: 'solid',
+        borderLeftStyle: 'solid',
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    secondLowestStringFret: {
+        borderBottomStyle: 'solid',
+    },
+    lowestStringFret: {
+        borderTopStyle: 'none',
+        borderLeftStyle: 'solid',
+        borderLeftColor: 'rgba(0, 0, 0, 0)',
+    },
+    secondHighestFret: {
+        borderRightStyle: 'solid',
+    },
+    highestFret: {
+        borderStyle: 'none',
+    },
+    noteMarker: {
+        position: 'absolute',
 
-		top: `${-markerDiameter / 2}rem`,
-		left: `${-fretWidth / 2 - markerDiameter / 2}rem`,
+        top: `${-markerDiameter / 2}rem`,
+        left: `${-fretWidth / 2 - markerDiameter / 2}rem`,
 
-		width: `${markerDiameter}rem`,
-		height: `${markerDiameter}rem`,
+        width: `${markerDiameter}rem`,
+        height: `${markerDiameter}rem`,
 
-		backgroundColor: 'red',
-		borderRadius: '100%',
-	},
+        backgroundColor: 'red',
+        borderRadius: '100%',
+    },
 };
--- src/chords.js
+++ src/chords.js
@@ -6,13 +6,13 @@ import { getNoteByDegree } from './scales';
 
 
 export function createChord(rootNote, chordType) {
-	const chordNotes = {
-		'chord_major': [0, 4, 7],
-		'chord_minor': [0, 3, 7],
-		'chord_dim': [0, 3, 6],
-	}[chordType];
+    const chordNotes = {
+        'chord_major': [0, 4, 7],
+        'chord_minor': [0, 3, 7],
+        'chord_dim': [0, 3, 6],
+    }[chordType];
 
-	return chordNotes;
+    return chordNotes;
 }
 
 export function getTriadFromScale(scale, degree) {
@@ -22,25 +22,25 @@ export function getTriadFromScale(scale, degree) {
 }
 
 export function getChordType(chordNotes) {
-	const rootNoteName = notes[chordNotes[0]];
+    const rootNoteName = notes[chordNotes[0]];
     const normalizedNotes = normalizeNotes(chordNotes);
 
     if (_.isEqual(normalizedNotes, [ 0, 4, 7 ])) {
         return {
-        	name: rootNoteName,
-        	type: 'chord_major',
+            name: rootNoteName,
+            type: 'chord_major',
         };
     }
     else if (_.isEqual(normalizedNotes, [ 0, 3, 7 ])) {
         return {
-        	name: `${rootNoteName}m`,
-        	type: 'chord_minor',
+            name: `${rootNoteName}m`,
+            type: 'chord_minor',
         };
     }
     else if (_.isEqual(normalizedNotes, [ 0, 3, 6 ])) {
         return {
-        	name: `${rootNoteName}dim`,
-        	type: 'chord_dim',
+            name: `${rootNoteName}dim`,
+            type: 'chord_dim',
         };
     }
 }
