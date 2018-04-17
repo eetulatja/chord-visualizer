879c69e338d3ae3369baeb324ae7685275bbb57e
Eetu Latja <eetu.latja@gmail.com>
2018-04-17 14:30:33 +0300

    Add drop D tuning

--- src/tunings.js
+++ src/tunings.js
@@ -27,6 +27,30 @@ const tunings = [
         },
     },
     {
+        id: 'tuning_guitar_6string_dropD',
+        title: 'Guitar 6-string drop D',
+        configuration: {
+            1: {
+                openNote: 53, // E4
+            },
+            2: {
+                openNote: 48, // B3
+            },
+            3: {
+                openNote: 44, // G3
+            },
+            4: {
+                openNote: 39, // D3
+            },
+            5: {
+                openNote: 34, // A2
+            },
+            6: {
+                openNote: 27, // D2
+            },
+        },
+    },
+    {
         id: 'tuning_bass_4string_standard',
         title: 'Bass 4-string standard',
         configuration: {
